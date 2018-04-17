863f9dfa12cc511a0bad5aaab7972632660ea0f7
Jess Hines <jess.hines@fmr.com>
2018-04-17 09:46:19 -0400

    Remove second sample test

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -3,11 +3,4 @@ describe("Home page", () => {
         cy.visit("http://localhost:4200")
         cy.contains("Welcome");
     })
-
-/*    it("goes to the login page", () => {
-        cy.visit("http://localhost:4200/#/welcome")
-        cy.contains("Log out").click();
-        cy.contains("Authorized use only");        
-    })
-*/
 })
